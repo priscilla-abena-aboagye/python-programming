ea39b8c314cc3a3f5111c71d42c4fbd2568dc19f
Abena <priscillaabenaaboagye@gmail.com>
2024-12-14 21:23:48 +0000

    Made it a little readable

--- chapter-3/discusision.py
+++ chapter-3/discusision.py
@@ -1,4 +1,4 @@
-
+# question 1
 a = 4.0 / 10.0 + 3.5 * 2
 print(a)
 print(type(a))
@@ -19,27 +19,44 @@ f = 3 ** 3
 print(f)
 print(type(f))
 
-d = sqrt(4.5 - 5.0) + 7 * 3
+d = math.sqrt(4.5 - 5.0) + 7 * 3
 print(d)
 print(type(d))
 print("sqrt is not an in-built function. You have to import math before it works")
 
-import math
-(3 + 4) * 5
-(n * (n - 1 )) / 2
-4 * math.pi * pow(r, 2)
-math.sqrt((r * (math.cos(a)**2)) + (r * (math.sin(b)**2)))
-((y ** 2) - (y ** 1)) / ((x ** 2) - (x **1))
 
+#question 2
+import math
+def main():
+    n = int(input("Enter a value for n: "))
+    r = int(input("Enter the value of the raduis: "))
+    x = int(input("nter a value for x: "))
+    y = int(input("nter a value for y: "))
+
+    (3 + 4) * 5
+    (n * (n - 1 )) / 2
+    a = 4 * math.pi * pow(r, 2)
+    b = math.sqrt((r * (math.cos(a)**2)) + (r * (math.sin(b)**2)))
+    c = ((y ** 2) - (y ** 1)) / ((x ** 2) - (x **1))
+    print(a, b, c)
+main()
+
+#question 3
 [0, 1, 2, 3, 4]
 [3, 4, 5, 6, 7, 8, 9]
 [4, 7, 10]
 [15, 13, 11, 9, 7]
 []
 
+#question4
 [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 [1, 27, 125, 343, 729]
 [0, 2, 4, 6, 8]  [12]  [done]
 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]      [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+#question 5
+#What do you think will happen if you use a negative number as the second parameter in the round function? 
 print("It will run it to the nearest multiple of 10")   [310]
+
+#question 6
 print("It will run down")  [-4]  [2]  [-4]  [2]  [3]
--- chapter-3/reviewquestions.py
+++ chapter-3/reviewquestions.py
@@ -1,14 +1,15 @@
-# a bhnjm   
+# This is a review question on python programming by John Zelle  
+# this is a true or false question
 
 '''
-True
-False
-False
-True
-True
-False
-True
-True
-True
-False
+Question1 = True
+Question2 = False
+Question3 = False
+Question4 = True
+Question5 = True
+Question6 = False
+Question7 = True
+Question8 = True
+Question9 = True
+Question10 = False
 '''
